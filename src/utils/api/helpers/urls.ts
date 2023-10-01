79c522f3e905f562c6c0c97812893ccb47d434b3
injurka <injurka@gmail.com>
2023-10-01 16:43:47 +0400

    feat: env

--- src/utils/api/helpers/urls.ts
+++ src/utils/api/helpers/urls.ts
@@ -1,6 +1,6 @@
 import type { Endpoints } from '#/types/common'
 
-const VITE_API_URL_PROD = import.meta.env.VITE_API_URL_PROD ?? 'http://localhost:8080'
+const VITE_API_URL_PROD = import.meta.env.VITE_API_URL_PROD ?? 'https://api.xsolare.pro'
 const VITE_API_URL_DEV = import.meta.env.VITE_API_URL_DEV ?? 'https://api.xsolare.pro'
 
 export const urls: Endpoints = {
